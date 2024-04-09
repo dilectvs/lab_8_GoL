264661f10880c82fc38e93befdbdf5ec186abe42
anthony <antonstupak246810@gmail.com>
2024-04-09 19:15:07 +0300

    initial commit

--- lab_8_1.py
+++ lab_8_1.py
@@ -11,20 +11,19 @@ def render_field(field):
         print ()
     print ('---------------')
 def render_pygame(field, scr):
-    scale = 20
+    scale = 15
     for y in range (0, len(field)):
         for x in range (0, len(field[0])):
             if field[y][x] == 0:
                 pygame.draw.rect(scr, (255, 255, 255), (x*scale, y*scale, scale, scale))
             elif field[y][x] == 1:
                 pygame.draw.rect(scr, (0, 255, 185), (x * scale, y * scale, scale, scale))
-                pygame.draw.rect(scr, (0, 0, 0), (x * scale, y * scale, scale, scale), 2)
+            pygame.draw.rect(scr, (0, 0, 0), (x * scale, y * scale, scale, scale), 1)
 def main():
     gof = life(30, 30)
     gof.initialize(30)
-
     pygame.init()
-    screen = pygame.display.set_mode ((600, 600))
+    screen = pygame.display.set_mode ((450, 450))
     pygame.display.set_caption("Game of Life")
     clock = pygame.time.Clock()
     is_running = True
